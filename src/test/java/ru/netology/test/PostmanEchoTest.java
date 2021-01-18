64d12373c9843ce0e23ccd12d3c253c4a1c772a3
Elena Fedoseeva <horungeelena@gmail.com>
2021-01-18 16:43:33 +0200

    Initial commit

---
+++ src/test/java/ru/netology/test/PostmanEchoTest.java
@@ -0,0 +1,24 @@
+package ru.netology.test;
+
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalToObject;
+
+public class PostmanEchoTest {
+    @Test
+    void shouldPostRequest() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalToObject("some data"))
+        ;
+    }
+}
